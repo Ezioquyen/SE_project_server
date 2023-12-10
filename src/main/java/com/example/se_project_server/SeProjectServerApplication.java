ed66561c68d89e2424a12c80fbd48b8bdae88e16
Ezioquyen <129573763+Ezioquyen@users.noreply.github.com>
2023-12-10 21:57:47 +0700

    Initial commit

---
+++ src/main/java/com/example/se_project_server/SeProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.se_project_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeProjectServerApplication.class, args);
+	}
+
+}
