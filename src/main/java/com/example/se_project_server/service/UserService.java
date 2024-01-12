595a9475d7ffe1865805476a015649f714ec6899
hoang <vmh06122003@gmail.com>
2024-01-12 20:56:32 +0700

    hoang

--- src/main/java/com/example/se_project_server/service/UserService.java
+++ src/main/java/com/example/se_project_server/service/UserService.java
@@ -1,7 +1,16 @@
 package com.example.se_project_server.service;
 
+import com.example.se_project_server.entity.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface UserService {
+    List<User> findAllUser ();
+    Optional<User> findUserById(int id);
+    boolean addUser(User user);
+    boolean updateUser(User user);
+    boolean deleteUserById(int id);
 }
--- src/main/java/com/example/se_project_server/service/UserServiceImp.java
+++ src/main/java/com/example/se_project_server/service/UserServiceImp.java
@@ -1,7 +1,60 @@
 package com.example.se_project_server.service;
 
+import com.example.se_project_server.entity.User;
+import com.example.se_project_server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
-public class UserServiceImp {
+public class UserServiceImp implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> findAllUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public boolean addUser(User user) {
+        if(userRepository.existsById(user.getId())){
+            return false;
+        }else{
+            userRepository.save(user);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean updateUser(User user) {
+        if(userRepository.existsById(user.getId())){
+            userRepository.saveAndFlush(user);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteUserById(int id) {
+        if(userRepository.existsById(id)){
+            userRepository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
