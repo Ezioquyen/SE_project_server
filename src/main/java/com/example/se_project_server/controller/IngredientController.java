5dd5bc87ecf1edc2ca25d545d275c30f73ed3247
VTzy <Truong.nv215495@sis.hust.edu.vn>
2024-01-13 11:22:51 +0700

    13/1/24

--- src/main/java/com/example/se_project_server/controller/IngredientController.java
+++ src/main/java/com/example/se_project_server/controller/IngredientController.java
@@ -2,8 +2,6 @@ package com.example.se_project_server.controller;
 
 import com.example.se_project_server.entity.Ingredient;
 import com.example.se_project_server.service.IngredientService;
-import com.example.se_project_server.service.IngredientServiceImp;
-import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,8 +20,6 @@ public class IngredientController {
     @PostMapping("/save")
     public void saveIngredient(@RequestBody Ingredient ingredient){
         System.out.println("add"+ingredient);
-//        JSONObject jsonObject = new JSONObject(ingredient);
-//        Ingredient ingredient1 = new Ingredient(jsonObject.getInt("id"), jsonObject.getString("name"), jsonObject.getInt("unit_Price"), jsonObject.getString("unit"));
         ingredientService.setIngredient(ingredient);
     }
     @DeleteMapping("/delete")
